bd3aa54f8a70b5990d093c8f81b8ebf6096b9ad6
nehak6296 <nehak6296@gmail.com>
2021-03-06 23:37:52 +0530

    [Neha]Add:Random function to check options

--- SnakeAndLadderGame/SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/Program.cs
@@ -4,14 +4,33 @@ namespace SnakeAndLadderGame
 {
     class Program
     {
-        static void Main(string[] args)
+        public const int NO_PLAY = 0;
+        public const int IS_LADDER = 1;
+        public const int IS_SNAKE = 2;
+        static void Main(string[]args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder Game...");
-
-            Console.WriteLine("Player is at starting position: START :Roll the Dice");
+            int playerPosition =0 ;
+              
             Random random = new Random();
-            int diceRoll = random.Next(1,7);
-            Console.WriteLine("Dice Number :"+diceRoll);
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine("Dice Number :" + diceRoll);
+
+            Random random1 = new Random();
+            int optionCheck = random1.Next(0, 3);
+            Console.WriteLine("Option " + optionCheck);
+            switch (optionCheck)
+            {
+                case NO_PLAY:
+                    playerPosition = 0;
+                    break;
+                case IS_LADDER:
+                   playerPosition += diceRoll;
+                    break;
+                case IS_SNAKE:
+                    playerPosition -= diceRoll;
+                    break;
+            }
+            Console.WriteLine("Player Position :" +playerPosition);
         }
     }
 }
