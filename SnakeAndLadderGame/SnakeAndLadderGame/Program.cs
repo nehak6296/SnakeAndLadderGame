cde56f9bcc1347a0c222b39332bf63c2077c83b9
nehak6296 <nehak6296@gmail.com>
2021-03-08 01:03:32 +0530

    [Neha]Add:playing game for two players

--- SnakeAndLadderGame/SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/Program.cs
@@ -7,7 +7,7 @@ namespace SnakeAndLadderGame
         public const int NO_PLAY = 0;
         public const int IS_LADDER = 1;
         public const int IS_SNAKE = 2;
-        public const int WINNING_POSITION = 10;
+        public const int WINNING_POSITION = 100;
         
 
         Random random = new Random();
@@ -21,51 +21,73 @@ namespace SnakeAndLadderGame
             int optionCheck = random.Next(0, 3);
             return optionCheck;
         }
+        public int Play(int playerPosition)
+        {            
+            Program program = new Program();
+            int diceRoll = program.DiceRoll();
+            Console.WriteLine("Dice" + diceRoll);
+            
+            int optionCheck = program.OptionCheck();
+            Console.WriteLine("Option " + optionCheck);
 
+
+            if (playerPosition + diceRoll > WINNING_POSITION)
+            {
+                optionCheck = NO_PLAY;
+            }
+
+            switch (optionCheck)
+            {
+                case NO_PLAY:
+                    break;
+                case IS_LADDER:
+                    playerPosition += diceRoll;
+                    break;
+                case IS_SNAKE:
+                    playerPosition -= diceRoll;
+                    break;
+            }
+            if ( optionCheck == IS_LADDER )
+            {
+                this.Play(playerPosition);
+            }
+            return playerPosition;
+        }
         static void Main(string[] args)
         {
-            int diceCount = 0;
+            
             Program program = new Program();
                         
-            int playerPosition = 0;
+            int playerOnePosition = 0;
+            int playerTwoPosition = 0;
+
                                    
             while ( true )
             {
-                int diceRoll = program.DiceRoll();
-                Console.WriteLine("Dice" + diceRoll);
-                diceCount++;
-                int optionCheck = program.OptionCheck();
-                Console.WriteLine("Option " + optionCheck);
-
-
-                if (playerPosition + diceRoll > WINNING_POSITION)
+                playerOnePosition = program.Play(playerOnePosition);
+                playerTwoPosition = program.Play(playerTwoPosition);
+                
+                if ( playerOnePosition == WINNING_POSITION )
+                {                    
+                    Console.WriteLine("Player One wins..");
+                    break;
+                }else if (playerTwoPosition == WINNING_POSITION)
                 {
-                    optionCheck = NO_PLAY;
-                }
-
-                switch (optionCheck)
-                {
-                    case NO_PLAY:
-                        break;
-                    case IS_LADDER:
-                        playerPosition += diceRoll;
-                        break;
-                    case IS_SNAKE:
-                        playerPosition -= diceRoll;
-                        break;
-                }
-                if ( playerPosition == WINNING_POSITION )
-                {
-                    Console.WriteLine("Number of times Dice Rolled : " + diceCount);
+                    Console.WriteLine("Player Two wins..");
                     break;
                 }
-                else if (playerPosition < 0 )
+                else if (playerOnePosition < 0 )
                 {
-                    playerPosition = 0;
+                    playerOnePosition = 0;
                 }
-                Console.WriteLine("Player Position :" + playerPosition);
+                else if (playerTwoPosition < 0)
+                {
+                    playerTwoPosition = 0;
+                }
+               // Console.WriteLine("Player Position :" + playerPosition);
             }
-            Console.WriteLine("Final Player Position :" + playerPosition);
+           // Console.WriteLine("Final Player Position :" + playerPosition);
         }
     }
 }
+
